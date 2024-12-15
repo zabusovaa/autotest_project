3265c3142cb3a5e44e1eba65fb3e91423cc80bea
Polina <zabusova12@mail.ru>
2024-12-16 00:47:47 +0300

    Реализация LoginPage

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from .locators import MainPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # проверка на корректный url адрес
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link.click()
+        assert "login" in self.browser.current_url, "Некорректный url, 'login' отсутствует"
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Форма входа отсутствует"
+
+
+    def should_be_register_form(self):
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Форма регистрации отсутствует"
--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -12,3 +13,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_login_should_be_in_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    link = "https://selenium1py.pythonanywhere.com/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
